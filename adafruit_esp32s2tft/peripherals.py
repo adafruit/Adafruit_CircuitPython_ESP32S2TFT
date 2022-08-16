ca977e040a71ff40e0880968e71bfe9c7053c097
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_esp32s2tft/peripherals.py
+++ adafruit_esp32s2tft/peripherals.py
@@ -32,7 +32,7 @@ from digitalio import DigitalInOut, Direction, Pull
 import neopixel
 
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_ESP32S2TFT.git"
 
 
