d7c25752c60c42275f0938dba83516051f6775af
foamyguy <foamyguy@gmail.com>
2025-05-16 15:19:59 +0000

    change to ruff

--- adafruit_esp32s2tft/network.py
+++ adafruit_esp32s2tft/network.py
@@ -28,14 +28,16 @@ Implementation Notes
 """
 
 import ssl
+
 from adafruit_portalbase.network import NetworkBase
 from adafruit_portalbase.wifi_esp32s2 import WiFi
 
 try:
-    from typing import Optional, Union, Callable
-    from neopixel import NeoPixel
-    from adafruit_io.adafruit_io import IO_MQTT
+    from typing import Callable, Optional, Union
+
     import adafruit_minimqtt.adafruit_minimqtt as MQTT
+    from adafruit_io.adafruit_io import IO_MQTT
+    from neopixel import NeoPixel
 except ImportError:
     pass
 
@@ -56,7 +58,6 @@ class Network(NetworkBase):
 
     """
 
-    # pylint: disable=too-many-instance-attributes, too-many-locals, too-many-branches, too-many-statements
     def __init__(
         self,
         *,
@@ -75,14 +76,13 @@ class Network(NetworkBase):
         """Initialize MQTT for Adafruit IO"""
         aio_username = self._get_setting["ADAFRUIT_AIO_USERNAME"]
         aio_key = self._get_setting["ADAFRUIT_AIO_KEY"]
-        if None in [aio_username, aio_key]:
+        if aio_username is None or aio_key is None:
             raise AttributeError(
                 "Adafruit IO keys are kept in settings.toml, please add them there."
             )
 
         return self.init_mqtt(IO_MQTT_BROKER, 8883, aio_username, aio_key, True)
 
-    # pylint: disable=too-many-arguments
     def init_mqtt(
         self,
         broker: str,
@@ -106,16 +106,12 @@ class Network(NetworkBase):
 
         return self._mqtt_client
 
-    # pylint: enable=too-many-arguments
-
     def _get_mqtt_client(self) -> Union[MQTT.MQTT, IO_MQTT]:
         if self._mqtt_client is not None:
             return self._mqtt_client
         raise RuntimeError("Please initialize MQTT before using")
 
-    def mqtt_loop(
-        self, *args: int, suppress_mqtt_errors: bool = True, **kwargs: int
-    ) -> None:
+    def mqtt_loop(self, *args: int, suppress_mqtt_errors: bool = True, **kwargs: int) -> None:
         """Run the MQTT Loop"""
         self._get_mqtt_client()
         if suppress_mqtt_errors:
@@ -126,9 +122,8 @@ class Network(NetworkBase):
                 print(f"MMQTTException: {err}")
             except OSError as err:
                 print(f"OSError: {err}")
-        else:
-            if self._mqtt_client is not None:
-                self._mqtt_client.loop(*args, **kwargs)
+        elif self._mqtt_client is not None:
+            self._mqtt_client.loop(*args, **kwargs)
 
     def mqtt_publish(
         self,
@@ -144,13 +139,10 @@ class Network(NetworkBase):
                     self._mqtt_client.publish(*args, **kwargs)
             except OSError as err:
                 print(f"OSError: {err}")
-        else:
-            if self._mqtt_client is not None:
-                self._mqtt_client.publish(*args, **kwargs)
+        elif self._mqtt_client is not None:
+            self._mqtt_client.publish(*args, **kwargs)
 
-    def mqtt_connect(
-        self, *args: Union[bool, str, int], **kwargs: Union[bool, str, int]
-    ) -> None:
+    def mqtt_connect(self, *args: Union[bool, str, int], **kwargs: Union[bool, str, int]) -> None:
         """Connect to MQTT"""
         self._get_mqtt_client()
         if self._mqtt_client is not None:
