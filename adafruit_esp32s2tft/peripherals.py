2b2e51c9ec6cc4ab8810285c76e87bb9b9ba04c0
Melissa LeBlanc-Williams <melissa@adafruit.com>
2025-06-12 13:33:15 -0700

    Fix peripherals file

--- adafruit_esp32s2tft/peripherals.py
+++ adafruit_esp32s2tft/peripherals.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: MIT
 """
-`adafruit_esp32s2tft`
+`adafruit_esp32s2tft.peripherals`
 ================================================================================
 
 Helper library for the Adafruit ESP32-S2 TFT Feather.
@@ -27,109 +27,61 @@ Implementation Notes
 
 """
 
-import gc
-
-from adafruit_portalbase import PortalBase
-
-from .graphics import Graphics
-from .network import Network
-from .peripherals import Peripherals
-
-try:
-    from typing import Callable, Dict, List, Optional, Sequence, Union
-
-    from neopixel import NeoPixel
-except ImportError:
-    pass
+import board
+import neopixel
+from digitalio import DigitalInOut, Direction, Pull
 
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_ESP32S2TFT.git"
 
 
-class ESP32S2TFT(PortalBase):
-    """Class representing the Adafruit ESP32-S2 TFT Feather.
+class Peripherals:
+    """Peripherals Helper Class for the ESP32S2TFT Library
 
-    :param url: The URL of your data source. Defaults to ``None``.
-    :param headers: The headers for authentication, typically used by Azure API's.
-    :param json_path: The list of json traversal to get data out of. Can be list of lists for
-                      multiple data points. Defaults to ``None`` to not use json.
-    :param regexp_path: The list of regexp strings to get data out (use a single regexp group). Can
-                        be list of regexps for multiple data points. Defaults to ``None`` to not
-                        use regexp.
-    :param default_bg: The path to your default background image file or a hex color.
-                       Defaults to 0x000000.
-    :param status_dotstar: The initialized object for status DotStar. Defaults to ``None``,
-                           to not use the status LED
-    :param json_transform: A function or a list of functions to call with the parsed JSON.
-                           Changes and additions are permitted for the ``dict`` object.
-    :param rotation: Default rotation is landscape (270) but can be 0, 90, or 180 for
-                     portrait/rotated
-    :param scale: Default scale is 1, but can be an integer of 1 or greater
-    :param debug: Turn on debug print outs. Defaults to False.
-    :param use_network: Enable network initialization. Defaults to True.
-                        Setting to False will allow you to use the library without a settings.toml
-                        file with wifi configuration in it.
 
+    Attributes:
+        neopixel (Neopixel): The on-board NeoPixel.
+            See https://circuitpython.readthedocs.io/projects/neopixel/en/latest/api.html
     """
 
-    def __init__(
-        self,
-        *,
-        url: Optional[str] = None,
-        headers: Dict[str, str] = None,
-        json_path: Optional[Union[List[str], List[List[str]]]] = None,
-        regexp_path: Optional[Sequence[str]] = None,
-        default_bg: int = 0,
-        status_neopixel: Optional[NeoPixel] = None,
-        json_transform: Optional[Union[Callable, List[Callable]]] = None,
-        rotation: int = 0,
-        scale: int = 1,
-        debug: bool = False,
-        use_network: bool = True,
-    ) -> None:
-        if use_network:
-            network = Network(
-                status_neopixel=status_neopixel,
-                extract_values=False,
-                debug=debug,
-            )
-        else:
-            network = None
+    def __init__(self) -> None:
+        # Neopixel
+        self.neopixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.3)
 
-        graphics = Graphics(
-            default_bg=default_bg,
-            rotation=rotation,
-            scale=scale,
-            debug=debug,
-        )
+        # Button
+        self._button = None
+        if hasattr(board, "BUTTON"):
+            # Only CircuitPython 7.2 or later has Button
+            self._button = DigitalInOut(board.BUTTON)
+            self._button.direction = Direction.INPUT
+            self._button.pull = Pull.UP
 
-        super().__init__(
-            network,
-            graphics,
-            url=url,
-            headers=headers,
-            json_path=json_path,
-            regexp_path=regexp_path,
-            json_transform=json_transform,
-            debug=debug,
-        )
+        # LED
+        self._led = DigitalInOut(board.LED)
+        self._led.switch_to_output()
 
-        self.peripherals = Peripherals()
+    def deinit(self) -> None:
+        """Call deinit on all resources to free them"""
+        self.neopixel.deinit()
+        self._button.deinit()
+        self._led.deinit()
 
-        gc.collect()
-
-    def enter_light_sleep(self, sleep_time: float) -> None:
+    @property
+    def button(self) -> bool:
         """
-        Enter light sleep and resume the program after a certain period of time.
-
-        See https://circuitpython.readthedocs.io/en/latest/shared-bindings/alarm/index.html for more
-        details.
-
-        :param float sleep_time: The amount of time to sleep in seconds
-
+        Return whether Down Button is pressed
         """
-        if self._alarm:
-            neopixel_value = self.peripherals.neopixel
-            super().enter_light_sleep(sleep_time)
-            self.peripherals.neopixel = neopixel_value
-            gc.collect()
+        if self._button:
+            return not self._button.value
+        return False
+
+    @property
+    def led(self) -> bool:
+        """
+        Return or set the value of the LED
+        """
+        return self._led.value
+
+    @led.setter
+    def led(self, value: bool) -> None:
+        self._led.value = bool(value)
